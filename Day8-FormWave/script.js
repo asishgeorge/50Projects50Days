fcbef567be0416da3834386e04b81808f5f62426
Asish George <asishag12@gmail.com>
2021-01-23 23:36:58 +0530

    Day 8: Input Wave Animation

---
+++ Day8-FormWave/script.js
@@ -0,0 +1,9 @@
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    console.log(label)
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx*50}ms">${letter}</span>`)
+        .join('')
+})
