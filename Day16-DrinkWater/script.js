5799f18393afb2805952079373e4e19349e3bcc5
Asish George <asishag12@gmail.com>
2021-02-14 16:50:52 +0530

    Day 16: drink water

---
+++ Day16-DrinkWater/script.js
@@ -0,0 +1,50 @@
+const smallCups = document.querySelectorAll('.small-cup');
+const litres = document.getElementById('litres')
+const remained = document.getElementById('remained')
+const percentage = document.getElementById('percentage')
+
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+
+    cup.addEventListener('click', () => {highlightCups(idx)});
+})
+
+function highlightCups(idx) {
+
+    if(idx === 7 && smallCups[idx].classList.contains('full') ) idx--;
+
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) idx--;
+   
+    smallCups.forEach((cup, idx2) => {
+        if(idx2 <= idx){
+            cup.classList.add('full')
+        }else{
+            cup.classList.remove('full')
+        }
+    })
+
+    updateBigCup();
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.small-cup.full').length;
+    const totalCups = smallCups.length;
+
+    if(fullCups === 0){
+        percentage.style.visibility='hidden';
+        percentage.style.height = 0;
+    }else{
+        percentage.style.visibility='visible';
+        percentage.style.height = `${fullCups/totalCups * 330}px`;
+        percentage.innerText= `${fullCups/totalCups * 100}%`
+    }
+
+    if(fullCups === totalCups) {
+        remained.style.visibility= 'hidden';
+        remained.style.height = 0;
+    }else{
+         remained.style.visibility= 'visible';
+         litres.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
+}
---
+++ Day16-DrinkWater/style.css
@@ -0,0 +1,92 @@
+@import url('https://fonts.googleapis.com/css?family=Montserrat:wght@400;700&display=swap');
+
+*{
+    box-sizing: border-box;
+}
+
+body{
+    background-color: rgb(132, 132, 233);
+    color:#fff;
+    font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100vh;
+}
+
+h1{
+    margin: 10px 0 0;
+}
+
+h3{
+    margin: 10px 0;
+}
+
+.cup{
+    background-color: #fff;
+    color:rgb(24, 24, 155);
+    border: 4px solid rgb(24, 24, 155);
+    border-radius: 0 0 40px 40px;
+    height: 330px;
+    width:150px;
+    margin: 30px 0;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    transition: 0.3s ease;
+}
+
+.cup.small-cup{
+    height: 95px;
+    width:50px;
+    border-radius: 0 0 15px 15px;
+    cursor:pointer;
+    background-color: rgba(255, 255, 255, 0.9);
+    font-size: 14px;
+    align-items: center;
+    justify-content: center;
+    margin:5px;
+    transition: 0.3s ease;
+}
+
+.cup.small-cup.full{
+    background-color: rgb(139, 139, 248);
+    color:#fff;
+}
+
+.cups{
+    display:flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content:center;
+    width:280px
+}
+
+.remained{
+    display:flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    text-align: center;
+    flex:1;
+    transition: 0.3s ease;
+}
+
+.remained span{
+    font-weight: bold;
+    font-size: 18px;
+}
+
+.percentage{
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: rgb(139, 139, 248);
+    color:#fff;
+    height:0;
+    font-size:20px;
+    font-weight: bold;
+    transition: 0.3s ease;
+}
